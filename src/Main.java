fa4799b83c169253662141a919194e189601857d
GrigoriiMartinkian <towaspas1@gmail.com>
2024-08-06 21:37:24 +0200

    my 4 commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!cvgbhcfvghjk,.m");
+        System.out.println("Hello world!cvgbhcfvhbjblmlghjk,dfghjk.m");
 
     }
 }
